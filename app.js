f49c303463734703d6af90fa51e3d929e1157b2e
Romain <dereuxromain@gmail.com>
2020-12-15 12:55:51 +0100

    add card

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ const circleMenu = document.querySelector('.circle-menu');
 btnCircle.addEventListener('click', ()=>{
     btnCircle.classList.toggle('menu-anim');
     circleMenu.classList.toggle('circle-anim');
-})
+});
+
+const allBlocs = document.querySelectorAll('.bloc');
+
+allBlocs.forEach(bloc =>{
+    bloc.addEventListener('click', (e)=>{
+        e.target.classList.add('active');
+        for(let i = 0; i < allBlocs.length; i++){
+            if(allBlocs[i] !== e.target){
+                allBlocs[i].classList.remove('active');
+            }
+        }
+    });
+});
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => btn.addEventListener('click', (e)=> {
+    e.stopPropagation();
+}));
